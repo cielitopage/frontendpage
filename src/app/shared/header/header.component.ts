7cfeb8722ae97ad3228494b010e129639393a404
leonardo arena <leonardofarena@gmail.com>
2023-11-01 13:13:42 -0300

    n

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+declare function customInitFunction(): any;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -15,6 +17,7 @@ export class HeaderComponent implements OnInit {
     
   ) { }
   ngOnInit(): void {
+    customInitFunction();
    
   }
 
