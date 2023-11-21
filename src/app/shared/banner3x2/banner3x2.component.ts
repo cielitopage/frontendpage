8f8c2d9db41edf43032828c84743df8f5a9f4c28
leonardo arena <leonardofarena@gmail.com>
2023-11-21 11:32:55 -0300

    nn

--- src/app/shared/banner3x2/banner3x2.component.ts
+++ src/app/shared/banner3x2/banner3x2.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner3x2',
   templateUrl: './banner3x2.component.html',
   styleUrls: ['./banner3x2.component.css']
 })
-export class Banner3x2Component {
+export class Banner3x2Component implements OnInit {
+
+  banner3x2: boolean = false;
+
+  banner3x2big :boolean = false;
+
+  constructor() { }
+
+
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.banner3x2 = true;
+    }, 3000);
+
+    setTimeout(() => {
+      this.banner3x2big = true;
+    }, 2000);
+  
+  
+  
+  }
+
+
+
+  //retrasar el banner 3x2 2 segundos
+
+
+
+
+
+
+
+
+
 
 }
