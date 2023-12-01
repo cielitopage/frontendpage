12cf431bc88741efe9cb0caf42bb48923e6c52e8
leonardo arena <leonardofarena@gmail.com>
2023-12-01 00:09:57 -0300

    n

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@
 export const environment = {
     production: true,
      
-     baseUrl: 'https://backendshopcielito-dev-kxct.2.us-1.fl0.io/api'
+     baseUrl: 'https://backendshopcielito-dev-aknm.2.ie-1.fl0.io/api'
     //baseUrl: 'https://backendshopcielito.vercel.app/api'
    //    baseUrl: 'http://localhost:3000/api',
 
