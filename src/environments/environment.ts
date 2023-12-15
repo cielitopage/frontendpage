07fa00afa5aa8bea128d324fc9e973794a670b5d
leonardo arena <leonardofarena@gmail.com>
2023-12-15 13:57:29 -0300

    nn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
     production: false,
        
  
-      // baseUrl: 'https://ejemolo.onrender.com/api'
-          baseUrl: 'http://localhost:3000/api',
+       baseUrl: 'https://ejemolo.onrender.com/api'
+        //  baseUrl: 'http://localhost:3000/api',
  
     
 };
